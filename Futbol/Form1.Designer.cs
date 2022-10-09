63df4e716ad370b2613817c166ed6451e86f9235
AliMusayevv <emusayev001@gmail.com>
2022-10-09 03:33:43 -0800

    Add project files.

---
+++ Futbol/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Futbol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.checkBoxGs = new System.Windows.Forms.CheckBox();
+            this.checkBoxFcb = new System.Windows.Forms.CheckBox();
+            this.checkBoxGarabag = new System.Windows.Forms.CheckBox();
+            this.checkBoxRma = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(314, 211);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(314, 211);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(652, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(314, 211);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 229);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(314, 211);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(332, 229);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(314, 211);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(652, 229);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(314, 211);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 446);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(314, 211);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(332, 446);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(314, 211);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(652, 446);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(314, 211);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGs
+            // 
+            this.checkBoxGs.AutoSize = true;
+            this.checkBoxGs.Location = new System.Drawing.Point(1311, 64);
+            this.checkBoxGs.Name = "checkBoxGs";
+            this.checkBoxGs.Size = new System.Drawing.Size(103, 20);
+            this.checkBoxGs.TabIndex = 9;
+            this.checkBoxGs.Text = "Galatasaray";
+            this.checkBoxGs.UseVisualStyleBackColor = true;
+            this.checkBoxGs.CheckedChanged += new System.EventHandler(this.checkBoxGs_CheckedChanged);
+            // 
+            // checkBoxFcb
+            // 
+            this.checkBoxFcb.AutoSize = true;
+            this.checkBoxFcb.BackColor = System.Drawing.Color.Transparent;
+            this.checkBoxFcb.Location = new System.Drawing.Point(1311, 12);
+            this.checkBoxFcb.Name = "checkBoxFcb";
+            this.checkBoxFcb.Size = new System.Drawing.Size(91, 20);
+            this.checkBoxFcb.TabIndex = 10;
+            this.checkBoxFcb.Text = "Barselona";
+            this.checkBoxFcb.UseVisualStyleBackColor = false;
+            this.checkBoxFcb.CheckedChanged += new System.EventHandler(this.checkBoxFcb_CheckedChanged);
+            // 
+            // checkBoxGarabag
+            // 
+            this.checkBoxGarabag.AutoSize = true;
+            this.checkBoxGarabag.Location = new System.Drawing.Point(1311, 90);
+            this.checkBoxGarabag.Name = "checkBoxGarabag";
+            this.checkBoxGarabag.Size = new System.Drawing.Size(83, 20);
+            this.checkBoxGarabag.TabIndex = 11;
+            this.checkBoxGarabag.Text = "Qarabağ";
+            this.checkBoxGarabag.UseVisualStyleBackColor = true;
+            this.checkBoxGarabag.CheckedChanged += new System.EventHandler(this.checkBoxGarabag_CheckedChanged);
+            // 
+            // checkBoxRma
+            // 
+            this.checkBoxRma.AutoSize = true;
+            this.checkBoxRma.Location = new System.Drawing.Point(1311, 38);
+            this.checkBoxRma.Name = "checkBoxRma";
+            this.checkBoxRma.Size = new System.Drawing.Size(103, 20);
+            this.checkBoxRma.TabIndex = 12;
+            this.checkBoxRma.Text = "Real Madrid";
+            this.checkBoxRma.UseVisualStyleBackColor = true;
+            this.checkBoxRma.CheckedChanged += new System.EventHandler(this.checkBoxRma_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 80;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1292, 568);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "0";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(972, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(314, 211);
+            this.button10.TabIndex = 14;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(972, 229);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(314, 211);
+            this.button11.TabIndex = 15;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(972, 445);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(314, 211);
+            this.button12.TabIndex = 16;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1292, 587);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(170, 70);
+            this.axWindowsMediaPlayer1.TabIndex = 17;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1474, 668);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxRma);
+            this.Controls.Add(this.checkBoxGarabag);
+            this.Controls.Add(this.checkBoxFcb);
+            this.Controls.Add(this.checkBoxGs);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Futbol";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.CheckBox checkBoxGs;
+        private System.Windows.Forms.CheckBox checkBoxFcb;
+        private System.Windows.Forms.CheckBox checkBoxGarabag;
+        private System.Windows.Forms.CheckBox checkBoxRma;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Futbol/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Futbol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int saniye = 0;
+
+        private void checkBoxFcb_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                timer1.Enabled = true;
+                checkBoxRma.Checked = false;
+                checkBoxGs.Checked = false;
+                checkBoxGarabag.Checked = false;
+                timer1.Enabled = true;
+                axWindowsMediaPlayer1.URL = "C:\\Users\\hp\\Desktop\\fcb.mp3";
+            }
+
+            catch
+            {
+                MessageBox.Show("Xəta baş verdi!");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                saniye = Convert.ToInt32(label1.Text);
+                saniye++;
+                label1.Text = saniye.ToString();
+
+                if (checkBoxFcb.Checked == true)
+                {
+
+
+
+
+                    if (saniye % 10 == 0)
+                    {
+                        button1.BackColor = Color.Red;
+                        button4.BackColor = Color.Red;
+                        button7.BackColor = Color.Red;
+                        button3.BackColor = Color.Red;
+                        button6.BackColor = Color.Red;
+                        button9.BackColor = Color.Red;
+                        button2.BackColor = Color.Blue;
+                        button5.BackColor = Color.Blue;
+                        button8.BackColor = Color.Blue;
+                        button10.BackColor = Color.Blue;
+                        button11.BackColor = Color.Blue;
+                        button12.BackColor = Color.Blue;
+
+                    }
+                    if (saniye % 10 == 5)
+                    {
+                        button1.BackColor = Color.Blue;
+                        button4.BackColor = Color.Blue;
+                        button7.BackColor = Color.Blue;
+                        button3.BackColor = Color.Blue;
+                        button6.BackColor = Color.Blue;
+                        button9.BackColor = Color.Blue;
+                        button2.BackColor = Color.Red;
+                        button5.BackColor = Color.Red;
+                        button8.BackColor = Color.Red;
+                        button10.BackColor = Color.Red;
+                        button11.BackColor = Color.Red;
+                        button12.BackColor = Color.Red;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+                }
+
+                if (checkBoxRma.Checked == true)
+                {
+
+
+
+
+                    if (saniye % 10 == 0)
+                    {
+                        button1.BackColor = Color.White;
+                        button4.BackColor = Color.White;
+                        button7.BackColor = Color.White;
+                        button3.BackColor = Color.White;
+                        button6.BackColor = Color.White;
+                        button9.BackColor = Color.White;
+                        button2.BackColor = Color.Black;
+                        button5.BackColor = Color.Black;
+                        button8.BackColor = Color.Black;
+                        button10.BackColor = Color.Black;
+                        button11.BackColor = Color.Black;
+                        button12.BackColor = Color.Black;
+
+                    }
+                    if (saniye % 10 == 5)
+                    {
+                        button1.BackColor = Color.Black;
+                        button4.BackColor = Color.Black;
+                        button7.BackColor = Color.Black;
+                        button3.BackColor = Color.Black;
+                        button6.BackColor = Color.Black;
+                        button9.BackColor = Color.Black;
+                        button2.BackColor = Color.White;
+                        button5.BackColor = Color.White;
+                        button8.BackColor = Color.White;
+                        button10.BackColor = Color.White;
+                        button11.BackColor = Color.White;
+                        button12.BackColor = Color.White;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+                }
+
+                if (checkBoxGarabag.Checked == true)
+                {
+
+
+
+
+                    if (saniye % 10 == 0)
+                    {
+                        button1.BackColor = Color.White;
+                        button4.BackColor = Color.White;
+                        button7.BackColor = Color.White;
+                        button3.BackColor = Color.White;
+                        button6.BackColor = Color.White;
+                        button9.BackColor = Color.White;
+                        button2.BackColor = Color.Black;
+                        button5.BackColor = Color.Black;
+                        button8.BackColor = Color.Black;
+                        button10.BackColor = Color.Black;
+                        button11.BackColor = Color.Black;
+                        button12.BackColor = Color.Black;
+
+                    }
+                    if (saniye % 10 == 5)
+                    {
+                        button1.BackColor = Color.Black;
+                        button4.BackColor = Color.Black;
+                        button7.BackColor = Color.Black;
+                        button3.BackColor = Color.Black;
+                        button6.BackColor = Color.Black;
+                        button9.BackColor = Color.Black;
+                        button2.BackColor = Color.White;
+                        button5.BackColor = Color.White;
+                        button8.BackColor = Color.White;
+                        button10.BackColor = Color.White;
+                        button11.BackColor = Color.White;
+                        button12.BackColor = Color.White;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+                }
+                if (checkBoxGs.Checked == true)
+                {
+
+
+
+
+                    if (saniye % 10 == 0)
+                    {
+                        button1.BackColor = Color.Yellow;
+                        button4.BackColor = Color.Yellow;
+                        button7.BackColor = Color.Yellow;
+                        button3.BackColor = Color.Yellow;
+                        button6.BackColor = Color.Yellow;
+                        button9.BackColor = Color.Yellow;
+                        button2.BackColor = Color.Red;
+                        button5.BackColor = Color.Red;
+                        button8.BackColor = Color.Red;
+                        button10.BackColor = Color.Red;
+                        button11.BackColor = Color.Red;
+                        button12.BackColor = Color.Red;
+
+                    }
+                    if (saniye % 10 == 5)
+                    {
+                        button1.BackColor = Color.Red;
+                        button4.BackColor = Color.Red;
+                        button7.BackColor = Color.Red;
+                        button3.BackColor = Color.Red;
+                        button6.BackColor = Color.Red;
+                        button9.BackColor = Color.Red;
+                        button2.BackColor = Color.Yellow;
+                        button5.BackColor = Color.Yellow;
+                        button8.BackColor = Color.Yellow;
+                        button10.BackColor = Color.Yellow;
+                        button11.BackColor = Color.Yellow;
+                        button12.BackColor = Color.Yellow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+                }
+            }
+            catch
+            {
+               
+            
+             MessageBox.Show("Xəta baş verdi!");
+            
+
+        }
+
+    }
+
+        private void checkBoxGs_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxFcb.Checked = false;
+            checkBoxRma.Checked = false;
+            checkBoxGarabag.Checked = false;
+            timer1.Enabled = true;
+            axWindowsMediaPlayer1.URL = "C:\\Users\\hp\\Desktop\\gs.mp3";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxRma_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            this.BackColor = Color.FromArgb(33, 55, 44);
+            checkBoxFcb.Checked = false;
+            checkBoxGs.Checked = false;
+            checkBoxGarabag.Checked = false;
+      
+            axWindowsMediaPlayer1.URL = "C:\\Users\\hp\\Desktop\\rma.mp3";
+
+
+
+        }
+
+        private void checkBoxGarabag_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            this.BackColor = Color.FromArgb(207, 228, 218);
+            checkBoxFcb.Checked = false;
+            checkBoxGs.Checked = false;
+            checkBoxRma.Checked = false;
+        
+            axWindowsMediaPlayer1.URL = "C:\\Users\\hp\\Desktop\\grb.mp3";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
